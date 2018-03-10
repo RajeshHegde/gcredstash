121c4ec67a540dd11027fe3251b966fbe0a2979d
Rajesh Hegde <rajesh.p.hegde@gmail.com>
2018-03-10 20:58:37 +0530

    add function comments  On branch develop 	modified:   gcredstash/keystore.py

--- gcredstash/keystore.py
+++ gcredstash/keystore.py
@@ -8,6 +8,12 @@ class KeyStore(object):
         self.client = datastore.Client(project=project_id, namespace=namespace)
 
     def get(self, kind, name):
+        """
+        Get value associated with the name from Datastore
+        :param kind: Collection name
+        :param name: Datastore key
+        :return: str
+        """
         key = self.client.key(kind, name)
 
         entity = self.client.get(key)
@@ -17,10 +23,23 @@ class KeyStore(object):
         return entity.get(KeyStore.CIPHER_PROPERTY_KEY)
 
     def put(self, kind, name, content):
+        """
+        Put value on the Datastore
+        :param kind: Collection name
+        :param name: Datastore key
+        :param content: value to store
+        :return:
+        """
         key = self.client.key(kind, name)
         entity = datastore.Entity(key=key, exclude_from_indexes=(KeyStore.CIPHER_PROPERTY_KEY,))
         entity[KeyStore.CIPHER_PROPERTY_KEY] = content
-        self.client.put(entity)
+        return self.client.put(entity)
 
     def list(self, kind):
+        """
+
+        :param kind: Collection name
+        :return: list of Datastore keys
+        """
+
         return [c.key.name for c in self.client.query(kind=kind).fetch()]
