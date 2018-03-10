0ba816435dbb7bf3a7ee9293de573aecffb7b771
Rajesh Hegde <rajesh.p.hegde@gmail.com>
2018-03-10 18:38:00 +0530

    command line tool  On branch develop 	new file:   google-credstash 	modified:   main.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,19 @@ from setuptools import setup
 
 setup(
     name='google-credstash',
+    author='Rajesh Hegde',
     version='1.0.0',
-    description='A utility for managing secrets in Google Cloud KMS and Datastore',
+    description='A Credential Management Tool using Google Cloud KMS and Datastore',
     url='https://github.com/RajeshHegde/google-credstash',
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
     ],
-    scripts=['main.py'],
+    scripts=['google-credstash'],
     install_requires=[
         'google-api-python-client',
         'google-cloud-datastore',
         'google-auth-httplib2',
         'python-dotenv',
     ],
-    entry_points={
-        'console_scripts': [
-            'google-credstash = google-credstash:main'
-        ]
-    }
 )
